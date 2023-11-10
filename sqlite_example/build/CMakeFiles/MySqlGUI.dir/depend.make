1a5e8c5d53e502a1363291eb8922702354ed98a0
anil-arise100 <anil.arise1600@gmail.com>
2023-11-10 14:29:36 +0530

    example & compile process add

--- sqlite_example/build/CMakeFiles/MySqlGUI.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for MySqlGUI.
-# This may be replaced when dependencies are built.
--- sqlite_example/build/CMakeFiles/sqlite_example.dir/sqlite_example_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sqlite_example.dir/sqlite_example_autogen/mocs_compilation.cpp.o: \
- /home/anil/ProjectsData/ExampleData/Help/qt_c++/SQLiteQuery/sqlite_example/build/sqlite_example_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
